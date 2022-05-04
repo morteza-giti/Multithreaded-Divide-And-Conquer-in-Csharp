258d63e1cbe4bce86889712d8add52e6f8ba1a95
Morteza Giti <mortezagiti1990@gmail.com>
2022-05-04 09:09:47 +0430

    final commit

---
+++ MultithreadedDivideAndConquer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace MultithreadedDivideAndConquer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var unsorted = new Queue<Queue<int>>();
+            long listLength = 10;
+			int maxSize = 1000;            
+            Random rand = new Random();
+            Console.WriteLine("Creating list of size " + listLength );
+            for (int i = 0; i < listLength; ++i)
+            {
+                var l = new Queue<int>();
+                l.Enqueue(rand.Next ( maxSize ));
+                unsorted.Enqueue(l);
+            }
+			Stopwatch parallelSW = new Stopwatch ();
+			Console.WriteLine ( "Starting Parallel MergeSort" );
+			var pending = new Queue<Queue<int>> ();
+			foreach ( Queue<int> q in unsorted )
+			{
+				pending.Enqueue ( new Queue<int> ( q ) );
+			}
+			parallelSW.Start ();
+			Queue<int> sortedParallel = Sorter.ParallelMergeSort ( pending );
+			parallelSW.Stop ();
+			Console.WriteLine ( "ParallelMergeSort - Time Elapsed: " + ( parallelSW.ElapsedMilliseconds ) + "ms" );
+			foreach ( int item in sortedParallel )
+			{
+				Console.WriteLine ( item );
+			}    
+            Console.Write("Press any key to exit...");
+            Console.Read();
+        }
+    }
+}
